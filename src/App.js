383910356f3d7665ce99d4bc7fcaa90c11a8e05b
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-13 19:35:38 -0300

    added more styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import { useState, useEffect } from "react";
 import "./styles.css";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  riseTitleText: {
+    backgroundColor: "#c0c0c0",
+    position: "absolute",
+    display: "inline",
+    top: 0,
+    left: 7,
+    textAlign: "center",
+    transform: "translateY(-50%)",
+    padding: "0 1px"
+  }
+});
 
 const sentenceTest = Array.from(
   "la casa de papel es una casa muy grande y delicada"
@@ -10,6 +24,7 @@ const BACKSPACE = "Backspace";
 const WPM = (typedEntries, mins) => typedEntries / 5 / mins;
 
 export default function App() {
+  const classes = useStyles();
   const [currentLetter, setCurrentLetter] = useState(0);
   const [keyPressed, setKeyPressed] = useState(null);
   const [startedTyping, setStartedTyping] = useState(false);
@@ -109,54 +124,66 @@ export default function App() {
           </span>
         ))}
       </div> */}
-        <div className="letters-row">
-          <Key isSelected={keyPressed === "q"} letter="Q" color="#fefec3" />
-          <Key isSelected={keyPressed === "w"} letter="W" color="#c4fcbf" />
-          <Key isSelected={keyPressed === "e"} letter="E" color="#c8fefc" />
-          <Key isSelected={keyPressed === "r"} letter="R" color="#fac5fb" />
-          <Key isSelected={keyPressed === "t"} letter="T" color="#fac5fb" />
-          <Key isSelected={keyPressed === "y"} letter="Y" color="#f991ff" />
-          <Key isSelected={keyPressed === "u"} letter="U" color="#f991ff" />
-          <Key isSelected={keyPressed === "i"} letter="I" color="#f991ff" />
-          <Key isSelected={keyPressed === "o"} letter="O" color="#c6f9c7" />
-          <Key isSelected={keyPressed === "p"} letter="P" color="#fdfcca" />
-        </div>
-        <div className="letters-row">
-          <Key isSelected={keyPressed === "a"} letter="A" color="#fefec3" />
-          <Key isSelected={keyPressed === "s"} letter="S" color="#c4fcbf" />
-          <Key isSelected={keyPressed === "d"} letter="D" color="#c8fefc" />
-          <Key isSelected={keyPressed === "f"} letter="F" color="#fac5fb" />
-          <Key isSelected={keyPressed === "g"} letter="G" color="#fac5fb" />
-          <Key isSelected={keyPressed === "h"} letter="H" color="#f991ff" />
-          <Key isSelected={keyPressed === "j"} letter="J" color="#f991ff" />
-          <Key isSelected={keyPressed === "k"} letter="K" color="#f991ff" />
-          <Key isSelected={keyPressed === "l"} letter="L" color="#c6f9c7" />
-          <Key isSelected={keyPressed === "ñ"} letter="Ñ" color="#fdfcca" />
-        </div>
-        <div className="letters-row">
-          <Key isSelected={keyPressed === "z"} letter="Z" color="#fefec3" />
-          <Key isSelected={keyPressed === "x"} letter="X" color="#c4fcbf" />
-          <Key isSelected={keyPressed === "c"} letter="C" color="#c8fefc" />
-          <Key isSelected={keyPressed === "v"} letter="V" color="#fac5fb" />
-          <Key isSelected={keyPressed === "b"} letter="B" color="#fac5fb" />
-          <Key isSelected={keyPressed === "n"} letter="N" color="#f991ff" />
-          <Key isSelected={keyPressed === "m"} letter="M" color="#f991ff" />
-        </div>
+
+        {/* Keyboard */}
         <div
-          className={keyPressed === " " ? "key-pressed" : ""}
           style={{
-            width: "12rem",
-            height: "2rem",
-            backgroundColor: "#ffccce",
-            padding: "0.2rem",
-            display: "flex",
-            justifyContent: "center",
-            border: "medium solid #8e807f",
-            borderRadius: "4px",
-            margin: "3px"
+            width: 570,
+            height: 205,
+            marginTop: 40,
+            border: "9px solid #808080"
           }}
-        />
+        >
+          <div className="letters-row">
+            <Key isSelected={keyPressed === "q"} letter="Q" color="#fefec3" />
+            <Key isSelected={keyPressed === "w"} letter="W" color="#c4fcbf" />
+            <Key isSelected={keyPressed === "e"} letter="E" color="#c8fefc" />
+            <Key isSelected={keyPressed === "r"} letter="R" color="#fac5fb" />
+            <Key isSelected={keyPressed === "t"} letter="T" color="#fac5fb" />
+            <Key isSelected={keyPressed === "y"} letter="Y" color="#f991ff" />
+            <Key isSelected={keyPressed === "u"} letter="U" color="#f991ff" />
+            <Key isSelected={keyPressed === "i"} letter="I" color="#f991ff" />
+            <Key isSelected={keyPressed === "o"} letter="O" color="#c6f9c7" />
+            <Key isSelected={keyPressed === "p"} letter="P" color="#fdfcca" />
+          </div>
+          <div className="letters-row">
+            <Key isSelected={keyPressed === "a"} letter="A" color="#fefec3" />
+            <Key isSelected={keyPressed === "s"} letter="S" color="#c4fcbf" />
+            <Key isSelected={keyPressed === "d"} letter="D" color="#c8fefc" />
+            <Key isSelected={keyPressed === "f"} letter="F" color="#fac5fb" />
+            <Key isSelected={keyPressed === "g"} letter="G" color="#fac5fb" />
+            <Key isSelected={keyPressed === "h"} letter="H" color="#f991ff" />
+            <Key isSelected={keyPressed === "j"} letter="J" color="#f991ff" />
+            <Key isSelected={keyPressed === "k"} letter="K" color="#f991ff" />
+            <Key isSelected={keyPressed === "l"} letter="L" color="#c6f9c7" />
+            <Key isSelected={keyPressed === "ñ"} letter="Ñ" color="#fdfcca" />
+          </div>
+          <div className="letters-row">
+            <Key isSelected={keyPressed === "z"} letter="Z" color="#fefec3" />
+            <Key isSelected={keyPressed === "x"} letter="X" color="#c4fcbf" />
+            <Key isSelected={keyPressed === "c"} letter="C" color="#c8fefc" />
+            <Key isSelected={keyPressed === "v"} letter="V" color="#fac5fb" />
+            <Key isSelected={keyPressed === "b"} letter="B" color="#fac5fb" />
+            <Key isSelected={keyPressed === "n"} letter="N" color="#f991ff" />
+            <Key isSelected={keyPressed === "m"} letter="M" color="#f991ff" />
+          </div>
+          <div
+            className={keyPressed === " " ? "key-pressed" : ""}
+            style={{
+              width: "12rem",
+              height: "2rem",
+              backgroundColor: "#ffccce",
+              padding: "0.2rem",
+              display: "flex",
+              justifyContent: "center",
+              border: "medium solid #8e807f",
+              borderRadius: "4px",
+              margin: "3px"
+            }}
+          />
+        </div>
       </div>
+
       <div
         style={{
           minWidth: "175px",
@@ -165,27 +192,276 @@ export default function App() {
           height: "100%",
           border: "2px solid",
           borderStyle: "inset",
-          padding: "5px"
+          padding: "13px 8px",
+          display: "flex",
+          gap: 15,
+          flexDirection: "column"
         }}
       >
-        <div style={{ border: "thin solid", borderStyle: "ridge" }}>
+        <div
+          style={{
+            border: "thin solid",
+            borderStyle: "ridge",
+            textAlign: "center",
+            fontSize: "0.85rem",
+            position: "relative",
+            height: 81
+          }}
+        >
+          <div className={classes.riseTitleText}>Alumno y nivel actual</div>
+          <br />
+          <div>
+            Lucas
+            <br />
+            {/* Aprendizaje
+            <br />
+            Lección 1 - Ejercicio 1 */}
+          </div>
+        </div>
+
+        <div
+          style={{
+            border: "thin solid",
+            borderStyle: "ridge",
+            textAlign: "center",
+            fontSize: "0.85rem",
+            position: "relative",
+            height: 81,
+            padding: 6,
+            paddingTop: 12
+          }}
+        >
+          <div className={classes.riseTitleText}>Incidencias</div>
           <div
             style={{
-              textAlign: "center",
-              fontSize: "0.9rem",
-              height: "100%"
+              backgroundColor: "#ff8080",
+              border: "2px solid",
+              borderStyle: "inset",
+              color: "white",
+              height: "100%",
+              width: "100%",
+              padding: 5
             }}
           >
-            <p className="rise-title-text">Alumno y nivel actual</p>
-            <br />
-            <div>
-              Lucas
-              <br />
-              Aprendizaje
-              <br />
-              Lección 1 - Ejercicio 1
+            Seleccione un ejercicio
+          </div>
+        </div>
+
+        <div
+          style={{
+            border: "thin solid",
+            borderStyle: "ridge",
+            textAlign: "center",
+            fontSize: "0.85rem",
+            position: "relative",
+            height: 67,
+            padding: 6,
+            paddingTop: 12
+          }}
+        >
+          <div className={classes.riseTitleText}>Valores establecidos</div>
+          <div style={{ display: "flex", flexDirection: "column", gap: 4 }}>
+            <div style={{ display: "flex", width: "100%", textAlign: "left" }}>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "70%"
+                }}
+              >
+                Coefi. m.e.p
+              </div>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "30%"
+                }}
+              >
+                2%
+              </div>
+            </div>
+
+            <div style={{ display: "flex", width: "100%", textAlign: "left" }}>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "70%"
+                }}
+              >
+                Velocidad
+              </div>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "30%"
+                }}
+              ></div>
             </div>
           </div>
+        </div>
+
+        <div
+          style={{
+            border: "thin solid",
+            borderStyle: "ridge",
+            textAlign: "center",
+            fontSize: "0.85rem",
+            position: "relative",
+            height: 150,
+            padding: 6,
+            paddingTop: 19
+          }}
+        >
+          <div className={classes.riseTitleText}>Resultados obtenidos</div>
+          <div style={{ display: "flex", flexDirection: "column", gap: 4 }}>
+            <div style={{ display: "flex", width: "100%", textAlign: "left" }}>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "70%"
+                }}
+              >
+                P. Brutas
+              </div>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "30%"
+                }}
+              ></div>
+            </div>
+            <div style={{ display: "flex", width: "100%", textAlign: "left" }}>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "70%"
+                }}
+              >
+                P. Netas
+              </div>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "30%"
+                }}
+              ></div>
+            </div>
+
+            <div style={{ display: "flex", width: "100%", textAlign: "left" }}>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "70%"
+                }}
+              >
+                Errores
+              </div>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "30%"
+                }}
+              ></div>
+            </div>
+
+            <div style={{ display: "flex", width: "100%", textAlign: "left" }}>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "70%"
+                }}
+              >
+                % Errores
+              </div>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "30%"
+                }}
+              ></div>
+            </div>
+
+            <div style={{ display: "flex", width: "100%", textAlign: "left" }}>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "70%"
+                }}
+              >
+                P. p. m.
+              </div>
+              <div
+                style={{
+                  background: "#e0e0e0",
+                  border: "2px solid",
+                  borderStyle: "inset",
+                  padding: "1px 4px",
+                  width: "30%"
+                }}
+              ></div>
+            </div>
+          </div>
+        </div>
+
+        <div style={{ display: "flex", width: "100%", padding: "0 7px" }}>
+          <div
+            style={{
+              background: "#e0e0e0",
+              border: "2px solid",
+              borderStyle: "inset",
+              padding: "1px 4px",
+              fontSize: "0.85rem",
+              width: "60%"
+            }}
+          >
+            Tiempo dis.
+          </div>
+          <div
+            style={{
+              background: "#e0e0e0",
+              border: "2px solid",
+              borderStyle: "inset",
+              width: "40%"
+            }}
+          ></div>
         </div>
       </div>
     </div>
@@ -203,7 +479,7 @@ function Key({ letter, color, isSelected = false }) {
         padding: "0.2rem",
         display: "flex",
         justifyContent: "center",
-        border: "medium solid #8e807f",
+        border: "3px solid #808080",
         borderRadius: "4px",
         margin: "3px"
       }}
