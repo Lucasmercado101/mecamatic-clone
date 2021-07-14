89fee4fb061eea4ae271759a8998951e330f334f
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-13 21:08:52 -0300

    revamping keyboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./styles.css";
 import { makeStyles } from "@material-ui/styles";
+// import {} from "ramda"
 
 const useStyles = makeStyles({
   riseTitleText: {
@@ -16,12 +17,26 @@ const useStyles = makeStyles({
 });
 
 const sentenceTest = Array.from(
-  "la casa de papel es una casa muy grande y delicada"
+  `Es indispensable que el alumno practique al menos media hora todos los días. Si el alumno tuviera necesidad de dominar la mecanografía rápidamente, deberá aumentar este tiempo a una o dos horas diarias, en cuyo caso el tiempo necesario para escribir con rapidez y precisión disminuirá proporcionalmente al tiempo y el esfuerzo empleado.
+  Lo que desde luego es imprescindible, es escribir todos los días, para adquirir cualquier habilidad se precisa tiempo y constancia.`
   // "la casa de casa la casa de casa la casa de casa la casa de casa la casa de casa la casa de casa la casa de casa"
 );
 
 const BACKSPACE = "Backspace";
 const WPM = (typedEntries, mins) => typedEntries / 5 / mins;
+
+const WelcomeMessage = () => (
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      display: "grid",
+      placeItems: "center"
+    }}
+  >
+    Bienvenido a MecaMatic 3.0
+  </div>
+);
 
 export default function App() {
   const classes = useStyles();
@@ -45,8 +60,9 @@ export default function App() {
   return (
     <div
       onKeyDown={(e) => {
-        const { key: keyPressed } = e;
+        const { key: keyPressed, code: keyCode } = e;
         setKeyPressed(keyPressed);
+        // console.log(e.code === "");
 
         if (currentLetter !== sentenceTest.length) {
           if (
@@ -95,16 +111,12 @@ export default function App() {
             // padding: 15
           }}
         >
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-              display: "grid",
-              placeItems: "center"
-            }}
-          >
-            Bienvenido a MecaMatic 3.0
-          </div>
+          {/* <WelcomeMessage /> */}
+          {sentenceTest.map((letter, i) => (
+            <span className={currentLetter === i ? "key-selected" : ""}>
+              {letter}
+            </span>
+          ))}
         </div>
         {/* <div
         style={{
@@ -118,11 +130,6 @@ export default function App() {
           padding: 15
         }}
       >
-        {sentenceTest.map((letter, i) => (
-          <span className={currentLetter === i ? "key-selected" : ""}>
-            {letter}
-          </span>
-        ))}
       </div> */}
 
         {/* Keyboard */}
@@ -131,56 +138,300 @@ export default function App() {
             width: 570,
             height: 205,
             marginTop: 40,
-            border: "9px solid #808080"
+            border: "9px solid #808080",
+            display: "flex",
+            flexDirection: "column",
+            gap: 4,
+            padding: 8,
+            fontWeight: "bold"
           }}
         >
-          <div className="letters-row">
-            <Key isSelected={keyPressed === "q"} letter="Q" color="#fefec3" />
-            <Key isSelected={keyPressed === "w"} letter="W" color="#c4fcbf" />
-            <Key isSelected={keyPressed === "e"} letter="E" color="#c8fefc" />
-            <Key isSelected={keyPressed === "r"} letter="R" color="#fac5fb" />
-            <Key isSelected={keyPressed === "t"} letter="T" color="#fac5fb" />
-            <Key isSelected={keyPressed === "y"} letter="Y" color="#f991ff" />
-            <Key isSelected={keyPressed === "u"} letter="U" color="#f991ff" />
-            <Key isSelected={keyPressed === "i"} letter="I" color="#f991ff" />
-            <Key isSelected={keyPressed === "o"} letter="O" color="#c6f9c7" />
-            <Key isSelected={keyPressed === "p"} letter="P" color="#fdfcca" />
+          <div style={{ display: "flex", gap: 4 }}>
+            <Key
+              style={{
+                width: 48,
+                fontSize: "1.1em",
+                textAlign: "left",
+                paddingLeft: 5,
+                paddingTop: 0
+              }}
+              letter="&#11134;"
+            />
+            <Key letter="Q" />
+            <Key letter="W" />
+            <Key letter="E" />
+            <Key letter="R" />
+            <Key letter="T" />
+            <Key letter="Y" />
+            <Key letter="U" />
+            <Key letter="I" />
+            <Key letter="O" />
+            <Key letter="P" />
+            <Key
+              letter={
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    position: "relative"
+                  }}
+                >
+                  <p style={{ position: "absolute", left: 5, top: 3 }}>^</p>
+                  <p style={{ position: "absolute", left: 6, top: 15 }}>`</p>
+                  <p
+                    style={{
+                      position: "absolute",
+                      left: 16,
+                      top: 10,
+                      fontSize: "0.7rem"
+                    }}
+                  >
+                    {"["}
+                  </p>
+                </div>
+              }
+            />
+            <Key
+              letter={
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    position: "relative"
+                  }}
+                >
+                  <p style={{ position: "absolute", left: 5, top: 2 }}>*</p>
+                  <p style={{ position: "absolute", left: 4, top: 9 }}>+</p>
+                  <p
+                    style={{
+                      position: "absolute",
+                      left: 18,
+                      top: 10,
+                      fontSize: "0.7rem"
+                    }}
+                  >
+                    {"]"}
+                  </p>
+                </div>
+              }
+            />
           </div>
-          <div className="letters-row">
-            <Key isSelected={keyPressed === "a"} letter="A" color="#fefec3" />
-            <Key isSelected={keyPressed === "s"} letter="S" color="#c4fcbf" />
-            <Key isSelected={keyPressed === "d"} letter="D" color="#c8fefc" />
-            <Key isSelected={keyPressed === "f"} letter="F" color="#fac5fb" />
-            <Key isSelected={keyPressed === "g"} letter="G" color="#fac5fb" />
-            <Key isSelected={keyPressed === "h"} letter="H" color="#f991ff" />
-            <Key isSelected={keyPressed === "j"} letter="J" color="#f991ff" />
-            <Key isSelected={keyPressed === "k"} letter="K" color="#f991ff" />
-            <Key isSelected={keyPressed === "l"} letter="L" color="#c6f9c7" />
-            <Key isSelected={keyPressed === "ñ"} letter="Ñ" color="#fdfcca" />
+
+          <div style={{ display: "flex", gap: 4 }}>
+            <Key
+              style={{
+                width: 59,
+                fontSize: "0.9rem",
+                textAlign: "left",
+                padding: 4
+              }}
+              letter="Mayús"
+            />
+            <Key letter="A" />
+            <Key letter="S" />
+            <Key letter="D" />
+            <Key
+              letter={
+                <div style={{ lineHeight: 0.3, paddingTop: 5 }}>
+                  F <br /> _
+                </div>
+              }
+            />
+            <Key letter="G" />
+            <Key letter="H" />
+            <Key
+              letter={
+                <div style={{ lineHeight: 0.3, paddingTop: 5 }}>
+                  J <br /> _
+                </div>
+              }
+            />
+            <Key letter="K" />
+            <Key letter="L" />
+            <Key letter="Ñ" />
+            <Key
+              letter={
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    position: "relative"
+                  }}
+                >
+                  <p style={{ position: "absolute", left: 6, top: 2 }}>¨</p>
+                  <p style={{ position: "absolute", left: 4, top: 13 }}>´</p>
+                  <p
+                    style={{
+                      position: "absolute",
+                      left: 16,
+                      top: 8,
+                      fontSize: "0.7rem"
+                    }}
+                  >
+                    {"{"}
+                  </p>
+                </div>
+              }
+            />
+            <Key
+              letter={
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    position: "relative"
+                  }}
+                >
+                  <p
+                    style={{
+                      position: "absolute",
+                      left: 5,
+                      top: -3,
+                      fontSize: "0.8rem"
+                    }}
+                  >
+                    ç
+                  </p>
+                  <p
+                    style={{
+                      position: "absolute",
+                      left: 16,
+                      top: 4,
+                      fontSize: "0.8rem"
+                    }}
+                  >
+                    }
+                  </p>
+                </div>
+              }
+            />
           </div>
-          <div className="letters-row">
-            <Key isSelected={keyPressed === "z"} letter="Z" color="#fefec3" />
-            <Key isSelected={keyPressed === "x"} letter="X" color="#c4fcbf" />
-            <Key isSelected={keyPressed === "c"} letter="C" color="#c8fefc" />
-            <Key isSelected={keyPressed === "v"} letter="V" color="#fac5fb" />
-            <Key isSelected={keyPressed === "b"} letter="B" color="#fac5fb" />
-            <Key isSelected={keyPressed === "n"} letter="N" color="#f991ff" />
-            <Key isSelected={keyPressed === "m"} letter="M" color="#f991ff" />
+
+          <div style={{ display: "flex", gap: 4 }}>
+            <Key
+              style={{
+                width: 44,
+                fontSize: "0.9rem",
+                textAlign: "left",
+                padding: 4
+              }}
+              letter="&#8679;"
+            />
+            <Key
+              letter={
+                <div style={{ lineHeight: 0.65 }}>
+                  &gt; <br /> &lt;
+                </div>
+              }
+            />
+            <Key letter="Z" />
+            <Key letter="X" />
+            <Key letter="C" />
+            <Key letter="V" />
+            <Key letter="B" />
+            <Key letter="N" />
+            <Key letter="M" />
+            <Key
+              letter={
+                <div
+                  style={{
+                    lineHeight: 0.65,
+                    fontSize: "0.85rem",
+                    textAlign: "left",
+                    paddingLeft: 6
+                  }}
+                >
+                  ;<br /> ,
+                </div>
+              }
+            />
+            <Key
+              letter={
+                <div
+                  style={{
+                    lineHeight: 0.65,
+                    fontSize: "0.85rem",
+                    textAlign: "left",
+                    paddingLeft: 6
+                  }}
+                >
+                  :<br /> .
+                </div>
+              }
+            />
+            <Key
+              letter={
+                <div
+                  style={{
+                    lineHeight: 1.2,
+                    fontSize: "0.9rem",
+                    textAlign: "left",
+                    paddingLeft: 6,
+                    marginTop: -8
+                  }}
+                >
+                  _<br /> -
+                </div>
+              }
+            />
+            <Key
+              style={{
+                width: 93,
+                fontSize: "0.9rem",
+                textAlign: "left",
+                padding: 4
+              }}
+              letter="&#8679;"
+            />
           </div>
-          <div
-            className={keyPressed === " " ? "key-pressed" : ""}
-            style={{
-              width: "12rem",
-              height: "2rem",
-              backgroundColor: "#ffccce",
-              padding: "0.2rem",
-              display: "flex",
-              justifyContent: "center",
-              border: "medium solid #8e807f",
-              borderRadius: "4px",
-              margin: "3px"
-            }}
-          />
+
+          {/* <div className="letters-row">
+              <Key isSelected={keyPressed === "q"} letter="Q" color="#fefec3" />
+              <Key isSelected={keyPressed === "w"} letter="W" color="#c4fcbf" />
+              <Key isSelected={keyPressed === "e"} letter="E" color="#c8fefc" />
+              <Key isSelected={keyPressed === "r"} letter="R" color="#fac5fb" />
+              <Key isSelected={keyPressed === "t"} letter="T" color="#fac5fb" />
+              <Key isSelected={keyPressed === "y"} letter="Y" color="#f991ff" />
+              <Key isSelected={keyPressed === "u"} letter="U" color="#f991ff" />
+              <Key isSelected={keyPressed === "i"} letter="I" color="#f991ff" />
+              <Key isSelected={keyPressed === "o"} letter="O" color="#c6f9c7" />
+              <Key isSelected={keyPressed === "p"} letter="P" color="#fdfcca" />
+            </div>
+            <div className="letters-row">
+              <Key isSelected={keyPressed === "a"} letter="A" color="#fefec3" />
+              <Key isSelected={keyPressed === "s"} letter="S" color="#c4fcbf" />
+              <Key isSelected={keyPressed === "d"} letter="D" color="#c8fefc" />
+              <Key isSelected={keyPressed === "f"} letter="F" color="#fac5fb" />
+              <Key isSelected={keyPressed === "g"} letter="G" color="#fac5fb" />
+              <Key isSelected={keyPressed === "h"} letter="H" color="#f991ff" />
+              <Key isSelected={keyPressed === "j"} letter="J" color="#f991ff" />
+              <Key isSelected={keyPressed === "k"} letter="K" color="#f991ff" />
+              <Key isSelected={keyPressed === "l"} letter="L" color="#c6f9c7" />
+              <Key isSelected={keyPressed === "ñ"} letter="Ñ" color="#fdfcca" />
+            </div>
+            <div className="letters-row">
+              <Key isSelected={keyPressed === "z"} letter="Z" color="#fefec3" />
+              <Key isSelected={keyPressed === "x"} letter="X" color="#c4fcbf" />
+              <Key isSelected={keyPressed === "c"} letter="C" color="#c8fefc" />
+              <Key isSelected={keyPressed === "v"} letter="V" color="#fac5fb" />
+              <Key isSelected={keyPressed === "b"} letter="B" color="#fac5fb" />
+              <Key isSelected={keyPressed === "n"} letter="N" color="#f991ff" />
+              <Key isSelected={keyPressed === "m"} letter="M" color="#f991ff" />
+            </div>
+            <div
+              className={keyPressed === " " ? "key-pressed" : ""}
+              style={{
+                width: "12rem",
+                height: "2rem",
+                backgroundColor: "#ffccce",
+                padding: "0.2rem",
+                display: "flex",
+                justifyContent: "center",
+                border: "medium solid #8e807f",
+                borderRadius: "4px",
+                margin: "3px"
+              }}
+            /> */}
         </div>
       </div>
 
@@ -468,20 +719,41 @@ export default function App() {
   );
 }
 
-function Key({ letter, color, isSelected = false }) {
+// function Key({ letter, color, isSelected = false }) {
+//   return (
+//     <div
+//       className={isSelected ? "key-pressed" : ""}
+//       style={{
+//         width: "2rem",
+//         height: "2rem",
+//         backgroundColor: color,
+//         padding: "0.2rem",
+//         display: "flex",
+//         justifyContent: "center",
+//         border: "3px solid #808080",
+//         borderRadius: "4px",
+//         margin: "3px"
+//       }}
+//     >
+//       {letter}
+//     </div>
+//   );
+// }
+
+function Key({ letter, style, ...otherProps }) {
   return (
     <div
-      className={isSelected ? "key-pressed" : ""}
+      // className={isSelected ? "key-pressed" : ""}
+      {...otherProps}
       style={{
-        width: "2rem",
-        height: "2rem",
-        backgroundColor: color,
-        padding: "0.2rem",
-        display: "flex",
-        justifyContent: "center",
+        width: 32,
+        height: 32,
         border: "3px solid #808080",
         borderRadius: "4px",
-        margin: "3px"
+        backgroundColor: "#e0e0e0",
+        textAlign: "center",
+        paddingTop: 3,
+        ...style
       }}
     >
       {letter}
