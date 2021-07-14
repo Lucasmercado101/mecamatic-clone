119c38927538b220e133d901c1e9dd46808b12b0
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-14 19:24:54 -0300

    added boilerplate setup

--- public/main.js
+++ public/main.js
@@ -11,6 +11,9 @@ function createWindow() {
   win.loadURL("http://localhost:3000/");
   win.setTitle("MecaMatic 3.0");
 
+  // Open the DevTools.
+  win.webContents.openDevTools();
+
   const menuTemplate = [
     {
       label: "Aprendizaje",
@@ -173,4 +176,25 @@ function createWindow() {
   Menu.setApplicationMenu(menu);
 }
 
-app.on("ready", createWindow);
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
