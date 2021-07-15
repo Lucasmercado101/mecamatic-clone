568ae562d39111a8064ee577cd5faa805142754a
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-15 12:21:50 -0300

    implemented model reducer stuff

--- src/model.js
+++ src/model.js
@@ -1,7 +1,29 @@
-export const initialModel = { areKeysColored: true };
-export const MESSAGES = {};
+import { cond, equals, assoc, pipe } from "ramda";
 
-export function modelReducer(state, action) {
-  switch (action) {
-  }
+const SENTENCE_POSITION = {
+  NOT_STARTED: -1,
+  FIRST_LETTER: 0
+};
+
+export const initialModel = {
+  areKeysColored: true,
+  exerciseSelected: null,
+  sentenceCursorPosition: SENTENCE_POSITION.NOT_STARTED
+};
+export const MESSAGES = {
+  LESSON_SELECTED: 0
+};
+
+export function modelReducer(state = initialModel, { type, payload }) {
+  const res = cond([
+    [
+      equals(MESSAGES.LESSON_SELECTED),
+      () =>
+        pipe(
+          assoc("exerciseSelected", payload),
+          assoc("sentenceCursorPosition", SENTENCE_POSITION.NOT_STARTED)
+        )(state)
+    ]
+  ]);
+  return res(type);
 }
