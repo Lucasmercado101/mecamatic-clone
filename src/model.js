ec1645b1e6ff195693a4464dc12bcce79b5ac97b
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-14 21:28:52 -0300

    default coloredKeys to false

--- src/model.js
+++ src/model.js
@@ -1,4 +1,4 @@
-export const initialModel = { areKeysColored: false };
+export const initialModel = { areKeysColored: true };
 export const MESSAGES = {};
 
 export function modelReducer(state, action) {
