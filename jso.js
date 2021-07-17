13672fa1afdf4dde6166eda548ff752c648d3018
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-17 11:12:41 -0300

    added practice - lesson 8

--- jso.js
+++ jso.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const lessonsPath = path.resolve(__dirname, "lessons", "practice", "lesson 7");
+const lessonsPath = path.resolve(__dirname, "lessons", "practice", "lesson 8");
 
 const lessonsSourcePath = path.resolve(__dirname, "lessons1");
 
