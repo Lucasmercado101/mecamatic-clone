dad5508dfb8fe351ad343d0f06804b5cc25c5355
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-17 09:16:55 -0300

    helper script to help convert lessons to json and converted more

---
+++ jso.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+const lessonsPath = path.resolve(__dirname, "lessons", "learning", "lesson 3");
+
+const lessonsSourcePath = path.resolve(__dirname, "lessons1");
+
+const dto = (text, isTutorActive, isKeyboardVisible) => ({
+  text,
+  isTutorActive,
+  isKeyboardVisible
+});
+
+const files = fs
+  .readdirSync(lessonsSourcePath)
+  .sort((a, b) => +a.split(".txt")[0] - +b.split(".txt")[0]);
+
+const finalJSONData = [];
+
+for (let i = 0; i < 10; i++) {
+  const file = files[i];
+  const filePath = path.join(lessonsSourcePath, file);
+  const content = fs.readFileSync(filePath, "utf-8");
+  finalJSONData.push(dto(content, true, true));
+}
+
+finalJSONData.forEach((el, i) => {
+  fs.writeFileSync(
+    path.join(lessonsPath, `${i + 1}.json`),
+    JSON.stringify(el),
+    "utf-8",
+    (err) => {}
+  );
+});
+
+// fs.readFile(path.join(lessonsPath), "utf-8", (err, data) => {
+//   });
