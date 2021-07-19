da44ca0e7c8fddeeab6496822033b928de133f8d
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-19 13:36:26 -0300

    added styles

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -25,36 +25,46 @@ function Welcome({
   }, []);
 
   return (
-    <div>
-      <input
-        list="user-profiles"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <datalist id="user-profiles">
-        {userNames.map((el, i) => (
-          <option value={el} key={i} />
-        ))}
-      </datalist>
-      <button
-        onClick={() => {
-          if (userNames.includes(userName)) {
-            electron.ipcRenderer
-              .invoke("load-user-profile", userName)
-              .then(({ userName }: userData) => {
-                send({ type: eventTypes.USER_DATA_LOADED, userName });
-              });
-          } else {
-            electron.ipcRenderer
-              .invoke("create-user-profile-and-load-user-it", userName)
-              .then(({ userName }: userData) => {
-                send({ type: eventTypes.USER_DATA_LOADED, userName });
-              });
-          }
-        }}
-      >
-        Aceptar
-      </button>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        padding: 15,
+        display: "grid",
+        placeItems: "center"
+      }}
+    >
+      <div style={{ display: "flex", gap: 15 }}>
+        <input
+          list="user-profiles"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <datalist id="user-profiles">
+          {userNames.map((el, i) => (
+            <option value={el} key={i} />
+          ))}
+        </datalist>
+        <button
+          onClick={() => {
+            if (userNames.includes(userName)) {
+              electron.ipcRenderer
+                .invoke("load-user-profile", userName)
+                .then(({ userName }: userData) => {
+                  send({ type: eventTypes.USER_DATA_LOADED, userName });
+                });
+            } else {
+              electron.ipcRenderer
+                .invoke("create-user-profile-and-load-user-it", userName)
+                .then(({ userName }: userData) => {
+                  send({ type: eventTypes.USER_DATA_LOADED, userName });
+                });
+            }
+          }}
+        >
+          Aceptar
+        </button>
+      </div>
     </div>
   );
 }
